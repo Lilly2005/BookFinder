b0f4d48c6da113b69473fbde72abc597d9814b55
Lilly2005 <chad.hanstedt@gmail.com>
2016-07-21 18:45:54 +0000

    incomplete

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,29 @@
+require './config/environment'
+require './app/models/model'
+
+
+class ApplicationController < Sinatra::Base
+
+# this helps connect our public and views directories
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+   erb :index
+  end
+  
+  get'/results' do
+    erb :results
+  end
+  
+ post '/results' do 
+    subject=params[:subject]
+    books = GoogleBooks.search('subject:#{subject}')
+    @book = books.first
+   erb :results 
+ end
+   
+
+end
